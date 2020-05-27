23ac618fa1895c54db4613fcc44d4bb5e1878d67
Larissa Morrell <larissa@larissamorrell.com>
2020-05-27 14:20:52 -0400

    animation card flip added

--- src/components/PlayingCard.js
+++ src/components/PlayingCard.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import ReactCardFlip from 'react-card-flip';
 import logo from '../assets/card_design.jpg';
 import './PlayingCard.css';
 
 const PlayingCard = ({ id, value, faceUp, handleClick }) => (
+  <div id="PlayingCardContainer">
+  <ReactCardFlip isFlipped={faceUp} flipDirection="horizontal">
     <div
-      className={`PlayingCard${faceUp ? " face-up-card" : ""}`}
+      className="PlayingCard"
       onClick={() => handleClick(id, value)}
     >
-      {faceUp ? <p>{value}</p> : <img src={logo} alt="face down card" />}
+      <img src={logo} alt="face down card" />
     </div>
+    <div
+      className="PlayingCard face-up-card"
+      onClick={() => handleClick(id, value)}
+    >
+      <p>{value}</p>
+    </div>
+  </ReactCardFlip>
+  </div>
   );
 
 export default PlayingCard;
