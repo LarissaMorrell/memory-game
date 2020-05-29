72331225b8222ea7fe51b419acdb35766088fdb3
Larissa Morrell <larissa@larissamorrell.com>
2020-05-29 12:17:23 -0400

    animation card flip, box-shadow, styles fixed for outline cutoff

--- src/components/PlayingCard.js
+++ src/components/PlayingCard.js
@@ -5,20 +5,20 @@ import './PlayingCard.css';
 
 const PlayingCard = ({ id, value, faceUp, handleClick }) => (
   <div id="PlayingCardContainer">
-  <ReactCardFlip isFlipped={faceUp} flipDirection="horizontal">
-    <div
-      className="PlayingCard"
-      onClick={() => handleClick(id, value)}
-    >
-      <img src={logo} alt="face down card" />
-    </div>
-    <div
-      className="PlayingCard face-up-card"
-      onClick={() => handleClick(id, value)}
-    >
-      <p>{value}</p>
-    </div>
-  </ReactCardFlip>
+    <ReactCardFlip isFlipped={faceUp} flipDirection="horizontal">
+      <div
+        className="PlayingCard"
+        onClick={() => handleClick(id, value)}
+      >
+        <img src={logo} alt="face down card" />
+      </div>
+      <div
+        className="PlayingCard face-up-card"
+        onClick={() => handleClick(id, value)}
+      >
+        <p>{value}</p>
+      </div>
+    </ReactCardFlip>
   </div>
   );
 
